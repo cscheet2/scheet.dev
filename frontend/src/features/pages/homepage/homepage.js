7af694cb16463a97602918e33678d06b73c78fed
Raegan Scheet <CamScheet@gmail.com>
2025-01-27 10:53:03 -0600

    set up homepage function signatures

--- frontend/src/features/pages/homepage/homepage.js
+++ frontend/src/features/pages/homepage/homepage.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+function Myself() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+function AboutMe() {
+  return (
+    <div>
+
+    </div>
+  ); 
+}
+
+function Highlights() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
 export function HomePage() {
   return (
     <div className="">:3</div>
