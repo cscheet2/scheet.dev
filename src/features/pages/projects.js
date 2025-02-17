38f5fde74eceebcd3554c2c27828b9ac4a716666
Raegan Scheet <CamScheet@gmail.com>
2025-02-17 17:24:53 -0600

    changed projects page placeholder text

--- src/features/pages/projects.js
+++ src/features/pages/projects.js
@@ -4,7 +4,7 @@ import Markdown from 'react-markdown';
 export default function ProjectsPage() {
   return (
     <div className='p-8 bg-slate-800 rounded-xl'>
-      :33
+      Under Construction...
     </div>
   );
 }
