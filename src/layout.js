238e7178c62de5f78ac02e92dd8195dd72b03273
Raegan Scheet <CamScheet@gmail.com>
2025-02-17 17:53:13 -0600

    phone screen margin

--- src/layout.js
+++ src/layout.js
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div id="portrait">
       <NavBar/>
-      <div className="p-10">
+      <div className="p-10 sm:p-4">
         <Outlet/>
       </div>
     </div>
