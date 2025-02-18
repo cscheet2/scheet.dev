8071fae405248d4456f2e39cbbfbeaaff1c440d0
Raegan Scheet <CamScheet@gmail.com>
2025-02-17 18:42:04 -0600

    lowered padding

--- src/layout.js
+++ src/layout.js
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div id="portrait">
       <NavBar/>
-      <div className="p-10 sm:p-4">
+      <div className="p-8">
         <Outlet/>
       </div>
     </div>
