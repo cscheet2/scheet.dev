49274f254aba20d10d7a8a7eb8f1d9743c74b648
Raegan Scheet <CamScheet@gmail.com>
2025-04-11 14:37:16 -0500

    Reorganize homepage layout and add more socials

--- src/features/pages/homepage.js
+++ src/features/pages/homepage.js
@@ -4,6 +4,8 @@ import ReactMarkdown from "react-markdown";
 import Raegan from "./../../images/Raegan.png";
 import GitHubImage from "./../../images/github-mark-white.png";
 import LinkedInImage from "./../../images/In-White-26.png"
+import InstagramImage from "./../../images/instagram-social-image.png";
+import EmailImage from "./../../images/email-social-image.png";
 
 import AboutMeText from "./../../documents/text/aboutme.md";
 
@@ -17,35 +19,58 @@ function Header() {
   );
 }
 
+function AboutMe({ className }) {
+  return (
+    <div className={`p-8 bg-slate-800 rounded-xl ${className}`}> 
+      <h2 className="text-3xl mb-4">Welcome!</h2>
+      <p className="leading-relaxed max-w-prose text-lg">
+        Hello, I'm Cameron! I'm pursuing a Bachelor of Science in Software Engineering at the University of Nebraska-Lincoln. 
+        My current interests include data visualization and robotics, but I’m always eager to explore new areas of computing. 
+        I’m currently working as a UCARE researcher in the Constraint Systems Laboratory at UNL, where I’m developing a MiniSAT visualization tool to enhance SAT education within the School of Computing. 
+        I’m seeking a Software Engineering internship in the Lincoln/Omaha area but am open to out-of-state opportunities for the summer.
+      </p>
+    </div>
+  ); 
+}
+
 function Myself({ className }) {
   return (
-    <div className={`p-8 pb-[72px] bg-slate-800 rounded-xl flex-0 w-auto h-full ${className}`}>
-        <img className="rounded-xl w-auto h-full" src={Raegan} alt="Scheet Dev"/>
-      <div className="pt-4 flex justify-evenly">
-        <button onClick={() => window.location.href = "https://github.com/cscheet2"}><img src={GitHubImage} className="w-10 h-10"/></button>
-        <button onClick={() => window.location.href = "https://www.linkedin.com/in/cameron-scheet/"}><img src={LinkedInImage} className="w-10 h-10"/></button>
-      </div>
+    // <div className={`flex flex-col justify-between items-center p-4 bg-slate-800 rounded-xl h-full ${className}`}>
+    //   <div className="flex justify-center">
+    //     <img className="rounded-xl w-auto max-h-[500px] object-cover mx-auto" src={Raegan} alt="Scheet Dev"/>
+    //   </div>
+    //   <div className="pt-4 flex justify-evenly w-full">
+    //     <button onClick={() => window.location.href = "https://github.com/cscheet2"}><img src={GitHubImage} className="w-10 h-10"/></button>
+    //     <button onClick={() => window.location.href = "https://www.linkedin.com/in/cameron-scheet/"}><img src={LinkedInImage} className="w-10 h-10"/></button>
+    //   </div>
+    // </div>
+    <div className={`p-8 bg-slate-800 rounded-xl ${className}`}>
+      <img className="rounded-xl w-auto max-h-[400px] object-cover mx-auto" src={Raegan} alt="Scheet Dev"/>
     </div>
   );
 }
 
-function AboutMe({ className }) {
-  const [about_me_text, set_about_me_text] = useState("");
-  useEffect(() => { fetch(AboutMeText).then(response => response.text()).then(text => set_about_me_text(text)); }, []);
+function Socials({ className }) {
   return (
-    <div className={`p-8 bg-slate-800 rounded-xl object prose ${className}`}>
-      <ReactMarkdown className="text-white">{about_me_text}</ReactMarkdown>
-    </div>
-  ); 
+      <div className={`p-8 bg-slate-800 rounded-xl flex flex-col justify-evenly ${className}`}>
+        <button onClick={() => window.location.href = "https://github.com/cscheet2"}><img src={GitHubImage} className="w-10 h-10"/></button>
+        <button onClick={() => window.location.href = "https://www.linkedin.com/in/cameron-scheet/"}><img src={LinkedInImage} className="w-10 h-10"/></button>
+        <button onClick={() => window.location.href = "https://www.instagram.com/camisdum"}><img src={InstagramImage} className="w-10 h-10"/></button>
+        <button onClick={() => window.location.href = "mailto:camscheet@gmail.com"}><img src={EmailImage} className="w-10 h-10"/></button>
+      </div>
+  );
 }
 
 export default function HomePage() {
   return (
     <div>
       <Header/>
-      <div className="flex flex-col lg:flex-row justify-center gap-8 lg:h-[calc(100svh-16em)]">
+      <div className="flex flex-col lg:flex-row gap-8 justify-center">
         <AboutMe className=""/>
-        <Myself className=""/>
+        <div className="flex flex-row gap-8 justify-center">
+          <Myself className=""/>
+          <Socials className=""/>
+        </div>
       </div>
     </div>
   );
